04d7c53161e74a38a5224ff7fa86db5c7020b899
Cindy Tammelin <100030995+CindyTammelin@users.noreply.github.com>
2025-11-07 22:02:08 +0000

    ok

--- src/app.js
+++ src/app.js
@@ -8,4 +8,30 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  function getRandomElement(arr) {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
+
+  function generateExcuse() {
+    let randomWho = getRandomElement(who);
+    let randomAction = getRandomElement(action);
+    let randomWhat = getRandomElement(what);
+    let randomWhen = getRandomElement(when);
+
+    let excuse = randomWho + ' ' + randomAction + ' ' + randomWhat + ' ' + randomWhen + '.';
+
+    const excuseElement = document.querySelector('#excuse');
+    if (excuseElement) {
+        excuseElement.innerHTML = excuse;
+    }
+  }
+
+  generateExcuse();
 };
